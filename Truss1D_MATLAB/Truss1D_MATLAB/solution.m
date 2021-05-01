2d7a115daabb04e6315b902c2f82f109644f9834
Dibakar58 <57082329+Dibakar58@users.noreply.github.com>
2021-05-01 15:57:33 +0530

    Delete Truss1D_MATLAB/Truss1D_MATLAB directory

--- Truss1D_MATLAB/Truss1D_MATLAB/solution.m
+++
@@ -1,7 +0,0 @@
-function disp = solution(nDof,fixedDof,K,force)
-%%%%% This function is to solve: K*U=F to obtain U
-activeDof = setdiff((1:nDof)',fixedDof);
-U = K(activeDof,activeDof)\force(activeDof);
-disp = zeros(nDof,1);
-disp(activeDof) = U;
-end
